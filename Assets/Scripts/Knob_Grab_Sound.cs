1723e25493605aec42102351c377b2f1bc106e7a
Anna Bertmark <annamy.bertmark@gmail.com>
2018-01-17 20:42:12 +0000

    Audio scripts for door creak and door knob inetraction
    Removed FMOD from project.
    Removed Steam Audio also from project.
    Fix for door interaction.
    Added audio scripts for door creak and door knob interaction (Grab and release) with placeholder sounds.

--- Assets/Scripts/Knob_Grab_Sound.cs
+++ Assets/Scripts/Knob_Grab_Sound.cs
@@ -7,24 +7,36 @@ using VRTK;
 public class Knob_Grab_Sound : MonoBehaviour {
 
 	public AudioSource soundEmitter;
-	public SoundCollection collection;
+	public SoundCollection grabSoundCollection;
+	public SoundCollection dropSoundCollection;
 
     private VRTK_InteractableObject interactable;
+
 
     void Start() {
 
         interactable = GetComponent<VRTK_InteractableObject> ();
 		interactable.InteractableObjectGrabbed += OnObjectGrabbed;
-	}
+        interactable.InteractableObjectUngrabbed += OnObjectDropped;
+    }
 		
 
 	void OnObjectGrabbed(object obj,InteractableObjectEventArgs args){
 
-		AudioClip clip  = collection.GetRandom ();
-
+		AudioClip clip  = grabSoundCollection.GetRandom ();
+        soundEmitter.volume = Random.Range(0.8f, 1);
+        soundEmitter.pitch = Random.Range(0.8f, 1);
         soundEmitter.PlayOneShot (clip);
 
     }
 
+	void OnObjectDropped(object obj,InteractableObjectEventArgs args){
+
+		AudioClip clip = dropSoundCollection.GetRandom ();
+        soundEmitter.volume = Random.Range(0.8f, 1);
+        soundEmitter.pitch = Random.Range(0.8f, 1);
+		soundEmitter.PlayOneShot (clip);
+
+	}
 }
 
