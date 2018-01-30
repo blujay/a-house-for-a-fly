b400fb697d03f6d0a4d6ea1a54e2602a342cd070
Maf'j Alvarez <jay@rootinteractive.com>
2018-01-30 17:18:00 +0000

    Added light trail bulbs and added to the cigar box with different colours - kind of like tilt-brush

---
+++ Assets/lina-belina/objects/Light-bulb/Scripts/IlluminateOnGrab.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class IlluminateOnGrab : MonoBehaviour {
+
+    private VRTK_InteractableObject interactable;
+    private Light bulbLight;
+    private TrailRenderer trail;
+    public bool trailsOn = false;
+
+
+    void Start()
+    {
+
+        interactable = GetComponent<VRTK_InteractableObject>();
+        interactable.InteractableObjectGrabbed += OnObjectGrabbed;
+        interactable.InteractableObjectUngrabbed += OnObjectDropped;
+        bulbLight = interactable.GetComponentInChildren<Light>();
+        trail = interactable.GetComponentInChildren<TrailRenderer>();
+    }
+
+
+    void OnObjectGrabbed(object obj, InteractableObjectEventArgs args)
+    {
+        bulbLight.enabled = true;
+        if (trailsOn) { trail.enabled = true; }
+        
+    }
+
+    void OnObjectDropped(object obj, InteractableObjectEventArgs args)
+    {
+        bulbLight.enabled = false;
+        if (!trailsOn) { trail.enabled = false; }
+
+    }
+
+}
