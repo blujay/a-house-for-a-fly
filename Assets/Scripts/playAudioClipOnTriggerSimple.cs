9154bd87245780320d65db8097e0aefb01ecde84
Maf'j Alvarez <jay@rootinteractive.com>
2018-02-06 17:46:01 +0000

    added sphere collider to linas head for triggering sound when object moved close.

---
+++ Assets/Scripts/playAudioClipOnTriggerSimple.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAudioClipOnTriggerSimple : MonoBehaviour {
+
+	public AudioClip[] ClipToPlay;
+    public AudioSource source;
+	public string GameTag;
+	bool played = false;
+		
+
+	void playSound(){
+		source.clip = ClipToPlay [Random.Range (0, ClipToPlay.Length)];
+		
+			source.Play ();
+			
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == GameTag) {
+			playSound();
+			//Debug.Log ("collided with " + GameTag);
+		}
+	}
+
+    void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == GameTag) {
+            source.Stop();
+        }
+	}
+
+}
---
+++ Assets/lina-belina/characters/animals/chicken/chickenStory.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chickenStory : MonoBehaviour
+{
+    public AudioSource audioS;
+    public AudioClip chickenStoryClip;
+    bool played = false;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("play sound now");
+            playSound();
+        }
+    }
+
+    void playSound()
+    {
+        if (!played)
+        {
+            audioS.PlayOneShot(chickenStoryClip);
+            played = true;
+        }
+        else
+        {
+            played = false;
+        }
+
+    }
+}
