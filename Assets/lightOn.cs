da7558cfc76944cc7ca410da62375347952732fb
Maf'j Alvarez <jay@rootinteractive.com>
2018-03-06 17:59:37 +0000

    Postprocessing and narration with timeline control of light and shutter

---
+++ Assets/lightOn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class lightOn : MonoBehaviour
+{
+
+    public Light roomLight;
+
+    void Start()
+    {
+        roomLight.enabled = false;
+        roomLight.color = Color.blue;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "GameController")
+        {
+            roomLight.enabled = true;
+        }
+    }
+}
