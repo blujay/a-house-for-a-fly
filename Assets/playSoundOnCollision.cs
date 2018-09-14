967e8f74cc99f0215a04fe81b0a56943fecfa240
Maf'j Alvarez <jay@rootinteractive.com>
2018-09-14 11:33:13 +0100

    Added nightmare scene with interactable cubes that will be thrown out of the window

---
+++ Assets/playSoundOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundOnCollision : MonoBehaviour
+{
+    AudioSource myaudio;
+    void Start()
+    {
+        myaudio = GetComponent<AudioSource>();
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "playsound")
+        {
+            myaudio.Play();
+            Debug.Log("How do you enjoy the sound?");
+        }
+    }
+}
---
+++ Assets/playSoundWhileOnCollision.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundWhileOnCollision : MonoBehaviour {
+    public GameObject nightmare;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = nightmare.GetComponent<AudioSource>();
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "nightmarecube") {
+            audioSource.Play();
+        }
+
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "nightmarecube")
+        {
+            audioSource.Play();
+        }
+        
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "nightmarecube")
+        {
+            audioSource.Stop();
+        }
+
+    }
+}
