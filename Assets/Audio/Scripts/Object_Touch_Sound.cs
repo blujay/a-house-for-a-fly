2e6bed7190a911245f99841826adfe4f0c53ca6e
AnnaMy82 <annamy.bertmark@gmail.com>
2018-10-01 21:43:59 +0100

    added Object_Touch-Sound script to play only once for Lila's reactions

---
+++ Assets/Audio/Scripts/Object_Touch_Sound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+
+public class Object_Touch_Sound : MonoBehaviour {
+
+	public AudioSource soundEmitter;
+	public SoundCollection touchSound;
+	public bool PlayOnlyOnce;
+
+
+	private VRTK_InteractableObject interactable;
+
+
+	void Start() {
+
+		interactable = GetComponent<VRTK_InteractableObject> ();
+		interactable.InteractableObjectTouched += OnObjectTouched;
+
+	}
+
+
+	void OnObjectTouched(object obj,InteractableObjectEventArgs args){
+
+		touchSound.Play (soundEmitter);
+		if (PlayOnlyOnce)
+			interactable.InteractableObjectTouched -= OnObjectTouched;
+
+	}
+
+
+
+}
